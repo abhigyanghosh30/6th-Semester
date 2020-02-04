b123a99d42f75e09dcec4bdd67a567c41ba0f948
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-02-05 02:04:15 +0530

    added stats

--- Linguistics Data 2/parse.py
+++ Linguistics Data 2/parse.py
@@ -5,6 +5,7 @@ nlp = stanfordnlp.Pipeline(processors='tokenize,pos,depparse', models_dir=MODELS
 f = open('corpus_revised_20171089.txt','r')
 f2 = open('parse.txt','w')
 f3 = open('hindi.output.txt','r')
+tags = {}
 lines = f.read()
 doc = nlp(lines)
 morph_lines = f3.readline()
@@ -14,11 +15,21 @@ for sentence in doc.sentences:
         f2.write(str(word.index.rjust(2))+"\t"+word.text+"\t"+word.xpos+"\t")
         f2.write("<"+",".join(morph[4:-1])+">\t")
         f2.write(str(word.governor)+"\t"+word.dependency_relation+"\n")
+        if word.dependency_relation in tags:
+            tags[word.dependency_relation]+=1
+        else:
+            tags[word.dependency_relation]=1
         morph_lines = f3.readline()
     f2.write("\n")
 f.close()
 f2.close()
 f3.close()
 
+new_dict = {key: value for key,value in sorted(tags.items(),key=lambda item:item[1])}
+
+f4 = open('stat.txt','w')
+for tag in new_dict:
+    f4.write(tag+":"+str(new_dict[tag])+"\n")
+f4.close
 # doc.sentences[0].print_dependencies()
 
