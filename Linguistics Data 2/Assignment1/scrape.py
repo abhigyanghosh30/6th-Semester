9d4c63536c8b6642d91959e5fde99fa5a4fbf76a
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-02-29 21:34:58 +0530

    added script to scrape data

--- Linguistics Data 2/Assignment1/scrape.py
+++ Linguistics Data 2/Assignment1/scrape.py
@@ -1,5 +1,5 @@
 import requests 
-
+from bs4 import BeautifulSoup
 
 payload = {'notation':'utf','out_notation':'utf','input':'जानवरों में गधा सबसे ज्यादा बुद्धिमान समझा जाता है'}
 
@@ -8,5 +8,11 @@ response = requests.post(
     data=payload,
     headers={'content-type':'application/x-www-form-urlencoded'}
 )
-response = requests.get(url='http://ltrc.iiit.ac.in/analyzer/hindi/all_out')
-output = response.text
+soup = BeautifulSoup(response.text, 'html.parser')
+outputs = soup.find_all('table')[1]
+print(outputs)
+
+for output in outputs.find_all('tr'):
+    if output.get('align') == 'left':
+        for content in output.contents:
+            print(content.replace(u'\xa0', u' '))
