f81c2c254a0f3535f68adb876e289799d1d01dce
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-03-18 01:23:47 +0530

    Till after Holi

---
+++ Intro to Game Theory/Assignment1/run1.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+import gambit
+import sys
+if len(sys.argv)<2:
+    print("Input format should be ./run input_file output_file")
+input_file=sys.argv[1]
+output_file=sys.argv[2]
+g = read`
