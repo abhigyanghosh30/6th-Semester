2755717f8904cfcc73deed4f97afdd42a603ffa0
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-04-03 15:49:56 +0530

    generate g, p done

---
+++ POIS/Evaluation1/sign.py
@@ -0,0 +1,65 @@
+import random
+import math
+
+def miillerTest(d, n): 
+      
+    a = 2 + random.randint(1, n - 4); 
+  
+    x = pow(a, d, n); 
+  
+    if (x == 1 or x == n - 1): 
+        return True; 
+  
+    while (d != n - 1): 
+        x = (x * x) % n; 
+        d *= 2; 
+  
+        if (x == 1): 
+            return False; 
+        if (x == n - 1): 
+            return True; 
+    return False; 
+
+def isPrime( n, k): 
+      
+    if (n <= 1 or n == 4): 
+        return False; 
+    if (n <= 3): 
+        return True; 
+  
+    d = n - 1; 
+    while (d % 2 == 0): 
+        d //= 2; 
+  
+    # Iterate given nber of 'k' times 
+    for i in range(k): 
+        if (miillerTest(d, n) == False): 
+            return False; 
+  
+    return True
+
+def generate(n):
+    print(n)
+    k = math.floor(math.sqrt(n/2))
+    q = random.getrandbits(n)
+    if isPrime(q,k):
+        print("q is prime")
+        p = 2*q+1
+        if isPrime(p,k):
+            print("p is prime")
+            g = random.randint(1,p-1)
+            if pow(g,q,p) != 1 and pow(g,2,p) != 1 :
+                print("g is generator")
+                return g,p
+            else:
+                return generate(n)
+        else:
+            return generate(n)
+    else:
+        return generate(n)
+
+if __name__ == "__main__":
+    print("Enter number of bits")
+    n = int(input())
+    g, p = generate(n)
+    print(g,p)
