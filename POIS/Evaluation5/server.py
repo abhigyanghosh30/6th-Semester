f3b953383a1a96ce34066cf65bd3b245c29907b4
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-05-05 08:39:34 +0530

    sem 6 over!

--- POIS/Evaluation5/server.py
+++ POIS/Evaluation5/server.py
@@ -19,22 +19,23 @@ s = socket.socket()
 port = params['port']
 s.bind(('', port))
 s.listen(5)
+
 while True:
     # try:
     c, addr = s.accept()
     R = str(c.recv(1024),encoding='ascii').split(';')
-    print(R)
+    print("Encrypted array from client",R)
     D = []
     for r in R:
         c_1 = int(r.split(',')[0])
         c_2 = int(r.split(',')[1])
         D.append(decrypt(c_1,c_2,x))
-    print(D)
+    print("Decrypted array",D)
     DB = []
     for i in range(k):
         DB.append(str(D[i]^data[i]))
 
-    print(DB)
+    print("Array sent by server",DB)
     c.send(bytes(",".join(DB),encoding='ascii'))
     # except:
     #     c.close()
